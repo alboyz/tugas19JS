17a67f7b67d6421021937882cb637aa91ff5f897
ali <alboyzskeaters@gmail.com>
2020-01-12 09:11:55 +0700

    Tugas19JS

---
+++ tugas19.js
@@ -0,0 +1,11 @@
+let kata = "Semangat...";
+console.log(kata.repeat(3));
+console.log("==========");
+let bintang="*"
+console.log(bintang.repeat(5));
+console.log(bintang.repeat(20));
+console.log(bintang.repeat(50));
+console.log(bintang.repeat(100));
+console.log(bintang.repeat(50));
+console.log(bintang.repeat(20));
+console.log(bintang.repeat(5));
